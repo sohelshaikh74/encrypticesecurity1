47b445dd4911d2e6d3a8f80141c0603d13da7cad
Sohel Shaikh <sohelshaikh30611@gmail.com>
2024-07-29 21:25:09 +0530

    All updated

--- js/course.js
+++ js/course.js
@@ -11,5 +11,12 @@ function toggleModule(moduleId) {
     }
 }
 
+document.querySelector('.show-login').addEventListener('click',function(){
+    document.querySelector('.popup').classList.add('active')
+ })
+ 
+ document.querySelector('.popup .close-btn').addEventListener('click',function(){
+    document.querySelector('.popup').classList.remove('active')
+ })
 
 // auto text write
