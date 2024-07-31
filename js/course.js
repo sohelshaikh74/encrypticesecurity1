11d54381327dfa599198f98dc1d7a76ba346eb0a
Sohel Shaikh <sohelshaikh30611@gmail.com>
2024-07-31 11:13:13 +0530

    updated

--- js/course.js
+++ js/course.js
@@ -1,22 +1,22 @@
-function toggleModule(moduleId) {
-    var content = document.getElementById(moduleId);
-    var icon = document.getElementById('icon-' + moduleId);
+// function toggleModule(moduleId) {
+//     var content = document.getElementById(moduleId);
+//     var icon = document.getElementById('icon-' + moduleId);
 
-    if (content.style.display === "none" || content.style.display === "") {
-        content.style.display = "block";
-        icon.textContent = "-";
-    } else {
-        content.style.display = "none";
-        icon.textContent = "+";
-    }
-}
+//     if (content.style.display === "none" || content.style.display === "") {
+//         content.style.display = "block";
+//         icon.textContent = "-";
+//     } else {
+//         content.style.display = "none";
+//         icon.textContent = "+";
+//     }
+// }
 
-document.querySelector('.show-login').addEventListener('click',function(){
-    document.querySelector('.popup').classList.add('active')
- })
+// document.querySelector('.show-login').addEventListener('click',function(){
+//     document.querySelector('.popup').classList.add('active')
+//  })
  
- document.querySelector('.popup .close-btn').addEventListener('click',function(){
-    document.querySelector('.popup').classList.remove('active')
- })
+//  document.querySelector('.popup .close-btn').addEventListener('click',function(){
+//     document.querySelector('.popup').classList.remove('active')
+//  })
 
-// auto text write
+// // auto text write
