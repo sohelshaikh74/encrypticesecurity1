2a7b0fab68ad70160ae8530d0209db6f57f9b979
Sohel Shaikh <sohelshaikh30611@gmail.com>
2024-08-13 16:29:49 +0530

    All updated

--- js/placement.js
+++ js/placement.js
@@ -2,24 +2,30 @@
 const contentData = [
     {
         before: { title: "Before", role: "Fresher" },
-        main: { img: "/img/placement/aniket.png", name: "Aniket Pawar", course: "EPGP in Data Science from IIITB<br>2019", hike: "60% Hike" },
-        after: { title: "After", role: "Data Scientist" }
+        main: { img: "/img/placement/pallav.png", name: "Pallav Purohit", course: "Offensive Security Certified Professional from EncrypticSecurity<br>2023", hike: "60% Hike" },
+        after: { title: "After", role: "Senior Cybersecurity Consultant" }
     },
     {
-        before: { title: "Before", role: "Analyst" },
-        main: { img: "/img/team/pravin-sir.jpg", name: "Anita Verma", course: "MBA in Data Science from ABC University<br>2020", hike: "70% Hike" },
-        after: { title: "After", role: "Senior Data Scientist" }
+        before: { title: "Before", role: "Fresher" },
+        main: { img: "/img/placement/aniket.png", name: "Aniket Pawar", course: "Advance Diploma in Cyber Security from EncrypticSecurity <br>2024", hike: "70% Hike" },
+        after: { title: "After", role: "Associate Cyber Security Consultant" }
     },
     {
-        before: { title: "Before", role: "Data Analyst" },
-        main: { img: "/img/team/shivam-sir.jpg", name: "Rohit Sharma", course: "PG Diploma in Data Science from XYZ Institute<br>2018", hike: "50% Hike" },
-        after: { title: "After", role: "Data Scientist Lead" }
+        before: { title: "Before", role: "Fresher" },
+        main: { img: "/img/placement/pratham.png", name: "Pratham mandavkar", course: "Master's of Cyber Security from EncrypticSecurity <br>2024", hike: "50% Hike" },
+        after: { title: "After", role: "Senior Analyst" }
     },
     {
-        before: { title: "Before", role: "System Engineer" },
-        main: { img: "image4.jpg", name: "Priya Desai", course: "MSc in Data Science from DEF College<br>2017", hike: "80% Hike" },
-        after: { title: "After", role: "AI Specialist" }
-    }
+        before: { title: "Before", role: "Fresher" },
+        main: { img: "/img/placement/shivam.png", name: "Shivam Babal", course: "Advance Diploma in Cyber Security from EncrypticSecurity <br>2024", hike: "50% Hike" },
+        after: { title: "After", role: "Associate Cyber Security Consultant" }
+    },
+    {
+        before: { title: "Before", role: "Fresher" },
+        main: { img: "/img/placement/rakshit.png", name: "Rakshit Gaikwad", course: "Advance Diploma in Cyber Security from EncrypticSecurity <br>2024", hike: "50% Hike" },
+        after: { title: "After", role: "Associate Information Security Consultant" }
+    },
+ 
 ];
 
 let currentIndex = 0;
@@ -27,13 +33,13 @@ let currentIndex = 0;
 function updateContent(index) {
     const data = contentData[index];
 
-    document.getElementById('before-content').innerHTML = `<h5>${data.before.title}</h5><p>${data.before.role}</p>`;
+    document.getElementById('before-content').innerHTML = `</h5><p>${data.before.role}</p>`;
     document.getElementById('profile-img').src = data.main.img;
     document.getElementById('main-name').innerText = data.main.name;
     document.getElementById('main-course').innerHTML = data.main.course;
     document.getElementById('main-hike').innerHTML = `<strong>${data.main.hike}</strong>`;
-    document.getElementById('after-content').innerHTML = `<h5>${data.after.title}</h5><p>${data.after.role}</p>`;
-    document.getElementById('current-slide').innerText = (index + 1) + '/4';
+    document.getElementById('after-content').innerHTML = `<p>${data.after.role}</p>`;
+    document.getElementById('current-slide').innerText = (index + 1) + '/5';
 }
 
 function prevContent() {
