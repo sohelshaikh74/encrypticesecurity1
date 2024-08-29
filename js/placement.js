f987a1d40bdacdbcf0151732e259f0f81ab35184
Sohel Shaikh <sohelshaikh30611@gmail.com>
2024-08-29 14:25:39 +0530

    All updated

--- js/placement.js
+++ js/placement.js
@@ -1,29 +1,30 @@
 // JavaScript for Content Carousel
 const contentData = [
     {
-        before: { title: "Before", role: "Fresher" },
+        before: { title: "Before", role: "FRESHER" },
         main: { img: "/img/placement/pallav.png", name: "Pallav Purohit", course: "Offensive Security Certified Professional from EncrypticSecurity<br>2023", hike: "100% Hike" },
-        after: { title: "After", role: "Senior Cybersecurity Consultant" }
+        after: { title: "After", role: "Senior Cybersecurity Consultant <br> -<strong>KPMG India Private Limited</strong>" }
+        // after: { title: "After", role: "123" }
     },
     {
-        before: { title: "Before", role: "Fresher" },
+        before: { title: "Before", role: "FRESHER" },
         main: { img: "/img/placement/aniket.png", name: "Aniket Pawar", course: "Advance Diploma in Cyber Security from EncrypticSecurity <br>2024", hike: "100% Hike" },
-        after: { title: "After", role: "Associate Cyber Security Consultant" }
+        after: { title: "After", role: "Associate Cyber Security Consultant <br> -<strong> ProTechmanize Solutions Pvt Ltd </strong>" }
     },
     {
-        before: { title: "Before", role: "Fresher" },
+        before: { title: "Before", role: "FRESHER" },
         main: { img: "/img/placement/pratham.png", name: "Pratham mandavkar", course: "Master's of Cyber Security from EncrypticSecurity <br>2024", hike: "100% Hike" },
-        after: { title: "After", role: "Senior Analyst" }
+        after: { title: "After", role: "Senior Analyst <br> -<strong> Capgemini Technology Services India Ltd </strong>" }
     },
     {
-        before: { title: "Before", role: "Fresher" },
+        before: { title: "Before", role: "FRESHER" },
         main: { img: "/img/placement/shivam.png", name: "Shivam Babal", course: "Advance Diploma in Cyber Security from EncrypticSecurity <br>2024", hike: "100% Hike" },
-        after: { title: "After", role: "Associate Cyber Security Consultant" }
+        after: { title: "After", role: "Associate Cyber Security Consultant <br> -<strong> ProTechmanize Solutions Pvt Ltd </strong>" }
     },
     {
-        before: { title: "Before", role: "Fresher" },
+        before: { title: "Before", role: "FRESHER" },
         main: { img: "/img/placement/rakshit.png", name: "Rakshit Gaikwad", course: "Advance Diploma in Cyber Security from EncrypticSecurity <br>2024", hike: "100% Hike" },
-        after: { title: "After", role: "Associate Information Security Consultant" }
+        after: { title: "After", role: "Associate Information Security Consultant <br>-<strong> Qseap Infotech Pvt Ltd </strong>" }
     },
  
 ];
